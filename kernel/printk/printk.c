f0bbdde854b6dede40e2b6b44e344e22d7b96513
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2021-09-20 06:22:08 +0600

    kernel: printk: Filter out some userspace logs
    Disables healthd and logd messages that flood most of the dmesg and they aren't useful.
    
    Co-authored-by: Divyanshu-Modi <divyan.m05@gmail.com>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- kernel/printk/printk.c
+++ kernel/printk/printk.c
@@ -708,6 +708,9 @@ static ssize_t devkmsg_write(struct kiocb *iocb, struct iov_iter *from)
 				facility = LOG_FACILITY(u);
 			endp++;
 			line = endp;
+			if (strstr(line, "healthd") ||
+				strncmp(line, "logd: Skipping", sizeof("logd: Skipping")))
+				return ret;
 		}
 	}
 
